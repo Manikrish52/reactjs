bf40be743ff186174a0f2eec1889b5d5f24c51ae
Manikrish52 <137646901+Manikrish52@users.noreply.github.com>
2023-08-07 12:31:10 +0530

    header changes

--- src/header/header.js
+++ src/header/header.js
@@ -1,44 +1,52 @@
 import React from 'react';
-
-import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import EuroIcon from '@mui/icons-material/Euro';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 
 function Header() {
   return (
     <Navbar expand="lg" className="rightside-navbar">
-      <Container>
-        <Navbar.Brand href="#home">Welcome you to Ecolife store!</Navbar.Brand>
+      
+        <div className='right-side'>
+        <Navbar.Brand className='rightside-title' href="#home">Welcome you to Ecolife store!</Navbar.Brand>
+        </div>
         {/*  */}
+        <div className='left-side'>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Compare (0)</Nav.Link>
-            <Nav.Link href="#link">Wishlist (0)</Nav.Link>
-            <NavDropdown title="Settings" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">My Account</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
+          <Nav className="  me-auto">
+        
+            <Nav.Link className='leftside-lables' href="#home">Compare (0) </Nav.Link>
+            <div className='wishlist-lable'>
+            <FavoriteBorderIcon className='hearticon' />
+            <Nav.Link className='leftside-lables' href="#link">Wishlist (0)</Nav.Link>
+            </div>
+            <NavDropdown  title="Settings" id="basic-nav-dropdown">
+              <NavDropdown.Item  href="#action/3.1">My Account</NavDropdown.Item>
+              <NavDropdown.Item  href="#action/3.2">
                 Clickout
               </NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Sign In</NavDropdown.Item>
               {/*  */}
             </NavDropdown>
                <NavDropdown title="English" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">English</NavDropdown.Item>
+              <NavDropdown.Item  href="#action/3.1">English</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.3">Francais</NavDropdown.Item>
             </NavDropdown>
             {/*  */}
-            <NavDropdown title="USD $" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">USD $</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                USD 
+            <NavDropdown  title="USD $" id="basic-nav-dropdown">
+              <NavDropdown.Item  href="#action/3.1">USD $</NavDropdown.Item>
+              <NavDropdown.Item  href="#action/3.2">
+                USD <EuroIcon className='uroicon'/> 
             </NavDropdown.Item>
                          
             </NavDropdown>
           </Nav>
         </Navbar.Collapse>
-      </Container>
+        </div>
+     
     </Navbar>
   );
 }

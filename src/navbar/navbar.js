00adaba87227d4ef44456c4de81ce83af6b431fe
Manikrish52 <137646901+Manikrish52@users.noreply.github.com>
2023-08-08 12:27:01 +0530

    header change

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -8,6 +8,7 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import SearchIcon from '@mui/icons-material/Search';
 import Card from 'react-bootstrap/Card';
 import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 function Navbarheader() {
   return (
@@ -28,58 +29,62 @@ function Navbarheader() {
           >
             {/* home */}
             
-           <NavDropdown title="Home" id="middle-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Home Organic</NavDropdown.Item>
+           <NavDropdown title="Home" id="middle-nav-dropdown" >
+            <div className='action-content'>
+              <NavDropdown.Item className='action-home' href="#action/1.1">Home Organic  <ArrowForwardIosIcon className='arrowforwardicion'/> </NavDropdown.Item> </div>
                <NavDropdown.Divider className='navdrop-divider' />
-              <NavDropdown.Item href="#action/3.2">
-                Home Cosmetic
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/1.2">
+                Home Cosmetic <ArrowForwardIosIcon className='arrowforwardicion'/></NavDropdown.Item>
                <NavDropdown.Divider className='navdrop-divider' />
-              <NavDropdown.Item href="#action/3.3">Home Digital</NavDropdown.Item>
+              <NavDropdown.Item href="#action/1.3">Home Digital <ArrowForwardIosIcon className='arrowforwardicion'/></NavDropdown.Item>
               <NavDropdown.Divider className='navdrop-divider' />
-              <NavDropdown.Item href="#action/3.4">
-                Home Furniture
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/1.4">
+                Home Furniture<ArrowForwardIosIcon className='arrowforwardicion'/></NavDropdown.Item>
                <NavDropdown.Divider className='navdrop-divider' />
-              <NavDropdown.Item href="#action/3.4">
-                Home Medical
-              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/1.5">
+                Home Medical<ArrowForwardIosIcon className='arrowforwardicion'/></NavDropdown.Item>
+             
             </NavDropdown>
                
                {/* shop */}
                <NavDropdown title="Shop" id="middle-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
+              <NavDropdown.Item href="#action/2.1">Action</NavDropdown.Item>
+               <NavDropdown.Divider className='navdrop-divider' />
+              <NavDropdown.Item href="#action/2.2">
+                Another action<ArrowForwardIosIcon className='arrowforwardicion'/>
               </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
+               <NavDropdown.Divider className='navdrop-divider' />
+              <NavDropdown.Item href="#action/2.3">Something</NavDropdown.Item>
+               <NavDropdown.Divider className='navdrop-divider' />
+              <NavDropdown.Item href="#action/2.4">
                 Separated link
-              </NavDropdown.Item>
+              </NavDropdown.Item> <NavDropdown.Divider className='navdrop-divider' />
             </NavDropdown>
                {/* pages */}
             <NavDropdown title="Pages" id="middle-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.1">About Page<ArrowForwardIosIcon className='arrowforwardicion'/></NavDropdown.Item>
+               <NavDropdown.Divider className='navdrop-divider' />
               <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
+                Cart Page <ArrowForwardIosIcon className='arrowforwardicion'/>
+              </NavDropdown.Item> <NavDropdown.Divider className='navdrop-divider' />
+              <NavDropdown.Item href="#action/3.3">Checkout Page <ArrowForwardIosIcon className='arrowforwardicion'/></NavDropdown.Item>
+               <NavDropdown.Divider className='navdrop-divider' />
+              <NavDropdown.Item href="#action/3.4">Compare Page <ArrowForwardIosIcon className='arrowforwardicion'/>         
+              </NavDropdown.Item>  <NavDropdown.Divider className='navdrop-divider' />
+              <NavDropdown.Item href="#action/3.5">Login & Regiter Page   <ArrowForwardIosIcon className='arrowforwardicion'/>       
+              </NavDropdown.Item>  <NavDropdown.Divider className='navdrop-divider' />
+              <NavDropdown.Item href="#action/3.6">Account Page <ArrowForwardIosIcon className='arrowforwardicion'/>
+              </NavDropdown.Item>  <NavDropdown.Divider className='navdrop-divider' />
+              <NavDropdown.Item href="#action/3.7">Wishlist Page <ArrowForwardIosIcon className='arrowforwardicion'/>         
               </NavDropdown.Item>
             </NavDropdown>
                {/* blog */}
           <NavDropdown title="Blog" id="middle-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
+              <NavDropdown.Item href="#action/3.5">Blog Grid <ArrowForwardIosIcon className='arrowforwardicion'/>       
+              </NavDropdown.Item>  <NavDropdown.Divider className='navdrop-divider' />
+              <NavDropdown.Item href="#action/3.6">Blog List <ArrowForwardIosIcon className='arrowforwardicion'/>
+              </NavDropdown.Item>  <NavDropdown.Divider className='navdrop-divider' />
+              <NavDropdown.Item href="#action/3.7">Blog Single<ArrowForwardIosIcon className='arrowforwardicion'/>         
               </NavDropdown.Item>
             </NavDropdown>
                {/* contact us */}

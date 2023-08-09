cd8ffedb33be4cc5e0d1977623affddc74ec6df6
Manikrish52 <137646901+Manikrish52@users.noreply.github.com>
2023-08-10 00:28:45 +0530

    header change

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -11,48 +11,82 @@ import Card from 'react-bootstrap/Card';
 import ShoppingBagOutlinedIcon from '@mui/icons-material/ShoppingBagOutlined';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import ListGroup from 'react-bootstrap/ListGroup';
-
+import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 
 function Navbarheader() {
   return (
       <Navbar expand="lg" className="navbarheader-content">
-      <Container fluid>
+      <Container fluid className='navbar-container'>
         <Navbar.Brand className='logo-content' href="#">
             
         </Navbar.Brand>
         
-        <Navbar.Toggle aria-controls="navbarScroll" />
+       <Navbar.Toggle aria-controls="navbarScroll" />
+       
         <Navbar.Collapse id="navbarScroll">
             {/*  */}
             <div className='navbar-content'>
           <Nav>
+               
+            
+
             {/* home */}
             
            <NavDropdown title="Home" id="middle-nav-dropdown"  >
-              <NavDropdown.Item className='action-home' href="#action/1.1">Home Organic   
+              <NavDropdown className='action-home' title='Home Organic' href="#action/1.1" id='homeorganic-content'>
+                <ListGroup className='home-organic'>
+                 <ListGroup.Item>Organic 1</ListGroup.Item>
+                 <ListGroup.Item>Organic 2</ListGroup.Item>
+                 <ListGroup.Item>Organic 3</ListGroup.Item>
+                 <ListGroup.Item>Organic 4</ListGroup.Item>
+                </ListGroup>      
                
-
-
               <ArrowForwardIosIcon className='arrowforwardicion'/> 
-              </NavDropdown.Item> 
+              </NavDropdown> 
               <NavDropdown.Divider className='navdrop-divider' />
-              <NavDropdown.Item className='action-home' href="#action/1.2">
-                Home Cosmetic <ArrowForwardIosIcon className='arrowforwardicion'/>
-                </NavDropdown.Item>
+              <NavDropdown className='action-home' href="#action/1.2" id='homeorganic-content' title='Home Cosmetic'>
+               <ListGroup className='home-organic'>
+               <ListGroup.Item>Cosmetic 1</ListGroup.Item>
+               <ListGroup.Item>Cosmetic 2</ListGroup.Item>
+               <ListGroup.Item>Cosmetic 3</ListGroup.Item>
+               <ListGroup.Item>Cosmetic 4</ListGroup.Item>
+                </ListGroup>
+                {/* <ArrowForwardIosIcon className='arrowforwardicion'/> */}
+                </NavDropdown>
                <NavDropdown.Divider className='navdrop-divider' />
-              <NavDropdown.Item className='action-home' href="#action/1.3">Home Digital <ArrowForwardIosIcon className='arrowforwardicion'/> 
-              </NavDropdown.Item>
-              <NavDropdown.Divider  className='navdrop-divider' />
-              <NavDropdown.Item className='action-home' href="#action/1.4">
-                Home Furniture<ArrowForwardIosIcon className='arrowforwardicion'/>
-                </NavDropdown.Item>
+               <NavDropdown className='action-home' href="#action/1.2" id='homeorganic-content' title='Home Digital'>
+               <ListGroup className='home-organic'>
+               <ListGroup.Item>Digital 1</ListGroup.Item>
+               <ListGroup.Item>Digital 2</ListGroup.Item>
+               <ListGroup.Item>Digital 3</ListGroup.Item>
+               <ListGroup.Item>Digital</ListGroup.Item>
+               </ListGroup>
+                {/* <ArrowForwardIosIcon className='arrowforwardicion'/> */}
+                </NavDropdown>
                <NavDropdown.Divider className='navdrop-divider' />
-              <NavDropdown.Item className='action-home' href="#action/1.5">
-                Home Medical<ArrowForwardIosIcon className='arrowforwardicion'/>
-                </NavDropdown.Item>
-             
-            </NavDropdown>
+              <NavDropdown className='action-home' href="#action/1.2" id='homeorganic-content' title='Home Furniture'>
+               <ListGroup className='home-organic'>
+               <ListGroup.Item>Furniture 1</ListGroup.Item>
+               <ListGroup.Item>Furniture 2</ListGroup.Item>
+               <ListGroup.Item>Furniture 3</ListGroup.Item>
+               <ListGroup.Item>Furniture 4</ListGroup.Item>
+               </ListGroup>
+                {/* <ArrowForwardIosIcon className='arrowforwardicion'/> */}
+                </NavDropdown>
+                <NavDropdown.Divider className='navdrop-divider' />
+               <NavDropdown className='action-home' href="#action/1.2" id='homeorganic-content' title='Home Medical'>
+               <ListGroup className='home-organic'>
+               <ListGroup.Item>Medical 1</ListGroup.Item>
+               <ListGroup.Item>Medical 2</ListGroup.Item>
+               <ListGroup.Item>Medical 3</ListGroup.Item>
+               <ListGroup.Item>Medical 4</ListGroup.Item>
+               </ListGroup>
+                {/* <ArrowForwardIosIcon className='arrowforwardicion'/> */}
+                  </NavDropdown>  
+                  <NavDropdown.Divider className='navdrop-divider' />
+                 <div className='single-product'>Single Product</div>
+                 </NavDropdown>
                
                {/* shop */}
                <NavDropdown title="Shop" id="middle-nav-dropdown">
@@ -119,25 +153,47 @@ function Navbarheader() {
             <NavDropdown title="Pages" id="middle-nav-dropdown">
                  <ListGroup className='page-lists'>
                  <ListGroup.Item className='page-uses'>About Page</ListGroup.Item>
+                 <NavDropdown.Divider className='navdrop-divider' />
                  <ListGroup.Item className='page-uses'>Cart Page</ListGroup.Item>
+                 <NavDropdown.Divider className='navdrop-divider' />
                  <ListGroup.Item className='page-uses'>Checkout Page</ListGroup.Item>
+                 <NavDropdown.Divider className='navdrop-divider' />
                  <ListGroup.Item className='page-uses'>Compare Page</ListGroup.Item>
+                 <NavDropdown.Divider className='navdrop-divider' />
                  <ListGroup.Item className='page-uses'>Login & Regiter Page</ListGroup.Item>
+                 <NavDropdown.Divider className='navdrop-divider' />
                  <ListGroup.Item className='page-uses'>Account Page</ListGroup.Item>
+                 <NavDropdown.Divider className='navdrop-divider' />
                  <ListGroup.Item className='page-uses'>Wishlist Page</ListGroup.Item>
                 </ListGroup>
             </NavDropdown>
                {/* blog */}
           <NavDropdown title="Blog" id="middle-nav-dropdown">
-              <NavDropdown.Item href="#action/3.5">Blog Grid <ArrowForwardIosIcon className='arrowforwardicion'/>       
-              </NavDropdown.Item>  <NavDropdown.Divider className='navdrop-divider' />
-              <NavDropdown.Item href="#action/3.6">Blog List <ArrowForwardIosIcon className='arrowforwardicion'/>
-              </NavDropdown.Item>  <NavDropdown.Divider className='navdrop-divider' />
-              <NavDropdown.Item href="#action/3.7">Blog Single<ArrowForwardIosIcon className='arrowforwardicion'/>         
-              </NavDropdown.Item>
+              <NavDropdown href="#action/3.5" title='Blog Grid' id='blog-title'>
+                <ListGroup.Item className='blog-content'>Blog Grid Left sidebar</ListGroup.Item>
+                <NavDropdown.Divider className='navdrop-divider' />
+               <ListGroup.Item className='blog-content'>Blog Grid Right Sidebar</ListGroup.Item>
+                 {/* <ArrowForwardIosIcon className='arrowforwardicion'/>        */}
+              </NavDropdown>
+               <NavDropdown.Divider className='navdrop-divider' />
+              <NavDropdown href="#action/3.6" title='Blog List' id='blog-title' placement='right' >
+                <ListGroup.Item className='blog-content'>Blog List Left sidebar</ListGroup.Item>
+                <NavDropdown.Divider className='navdrop-divider' />
+               <ListGroup.Item className='blog-content'>Blog List Right Sidebar</ListGroup.Item>
+                 {/* <ArrowForwardIosIcon className='arrowforwardicion'/> */}
+              </NavDropdown>  
+              <NavDropdown.Divider className='navdrop-divider' />
+
+              <NavDropdown href="#action/3.7" title='Blog Single' id='blog-title'>
+                <ListGroup.Item className='blog-content'>Blog Single Left Sidebar</ListGroup.Item>
+                <NavDropdown.Divider className='navdrop-divider' />
+               <ListGroup.Item className='blog-content'>Blog Single Right Sidebar</ListGroup.Item>
+                {/* <ArrowForwardIosIcon className='arrowforwardicion'/>          */}
+              </NavDropdown>
             </NavDropdown>
                {/* contact us */}
                  <div className='contact-content'>Contact</div>
+                
                    {/* search */}
         
          <div className='search-containt'>
@@ -165,7 +221,9 @@ function Navbarheader() {
             </div>
          <div className='call-content'>
             <Nav.Link > 
-            <div className='amount-content'> <ShoppingBagOutlinedIcon className='shoppingbag'/>$20.00
+            <div className='amount-content'> <ShoppingBagOutlinedIcon className='shoppingbag'  /><div className='number-two'>2</div>
+            
+            <div className='number-content'>$20.00</div>
             
             </div>
                 </Nav.Link>

d5d5ed555672f5496d3ba657543da59f39c16b7f
Manikrish52 <137646901+Manikrish52@users.noreply.github.com>
2023-08-12 01:27:51 +0530

    header change

--- src/sliderarea/slider.js
+++ src/sliderarea/slider.js
@@ -7,21 +7,24 @@ import SliderImg2 from '../sliderarea/sliderimage/sample-2.webp'
 
 function Slider() {
   return (
-        <Carousel data-bs-theme="dark">
+        <Carousel data-bs-theme="dark" className='carousel-content'>
       <Carousel.Item>
         <img
+        id='firstimage-content'
           className="slider1"
           src={SliderImg}
           alt="First slide"
         />
         <Carousel.Caption className='firstslide_content'>
-          <h5>NOT FRIED NOT BAKED</h5>
+          <div className='firstcarousel-title'>
+          <h5 className='top'>NOT FRIED NOT BAKED</h5>
           <div>
-           <p> Freeze Dried Friits
+           <p className='middle'> Freeze Dried Friits
             <br/>
             Pineapple Coconut
            </p>
-           <p>Free  Shipping On Qualified Orders $35</p>
+           <p className='bottom'>Free  Shipping On Qualified Orders $35</p>
+            </div>
             </div>
         </Carousel.Caption>
       </Carousel.Item>
@@ -32,9 +35,11 @@ function Slider() {
           alt="Second slide"
         />
         <Carousel.Caption className='secondslide_content'>
-          <h5>100% NATURAL</h5>
-          <p>Organic Fruits <br/> Summer Drinks</p>
-          <p>Fresh New pack Brusting Fruits</p>
+          <div className='secondcarousel-title'>
+          <h5 className='top'>100% NATURAL</h5>
+          <p className='middle'>Organic Fruits <br/> Summer Drinks</p>
+          <p className='bottom'>Fresh New pack Brusting Fruits</p>
+          </div>
         </Carousel.Caption>
       </Carousel.Item>
     

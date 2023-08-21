66f392ae964a96f18369bc3642966a7a41cad0f5
Manikrish52 <137646901+Manikrish52@users.noreply.github.com>
2023-08-22 00:38:17 +0530

    first seller end

--- src/Seller/seller.js
+++ src/Seller/seller.js
@@ -3,7 +3,11 @@ import '../Seller/seller.css'
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import StarIcon from '@mui/icons-material/Star';
+import EuroIcon from '@mui/icons-material/Euro';
+import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
+import ShuffleIcon from '@mui/icons-material/Shuffle';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
+import SearchIcon from '@mui/icons-material/Search';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import Sellerimg1 from '../Seller/Sellerimg/product-1.webp'
 function Seller() {
@@ -24,23 +28,35 @@ function Seller() {
         </div>
         {/* seller product */}
         <div className='sellerfirst-box'>
-            <Card >
+            <Card id='card-items' >
+       <div className='product-img'>
       <Card.Img variant="top"
       className='manago-img'
-      src={Sellerimg1} />
-      <Card.Body>
-        <h4 className='product-title'>STUDIO DESIGN</h4>
-        <p className='product-use'>Juicy Couture Juicy <br/>Quilted Ter..</p>
-        <div> <StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/></div>
-        <Card.Text>
-
-        </Card.Text>
       
+      src={Sellerimg1} 
+    
+      /> <div className='product-search'> <SearchIcon className='search-icon'/></div>
+      </div>
+      <Card.Body className='card-product'>
+        <h4 className='product-title'>STUDIO DESIGN</h4>
+        <p className='product-use'>Juicy Couture Juicy Quilted Ter..</p>
+        <div> <StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/></div>
+        <div className='product-rate'><EuroIcon className='product-euroicon'/>18.90 <span className='secondproduct-rate'><EuroIcon className='product-euroicon'/>34.21</span></div>
+        <div className='product-footer'>
+          <div className='add-cart'>ADD TO CART</div>
+          <div className='leftside-icon'>
+            <ShuffleIcon  className='cross-icon'/> &nbsp;<FavoriteBorderIcon  className='heart-icon'/>
+          </div>
+        </div>
       </Card.Body>
+      
     </Card>
         </div>
-    </div>
+       
+    </div> 
     // seller products
+
+   
   )
 }
 

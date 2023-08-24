5cfcc7ebfd65547654f65d6526e2507abd6c0366
Manikrish52 <137646901+Manikrish52@users.noreply.github.com>
2023-08-24 23:59:24 +0530

    scroller set

--- src/Seller/seller.js
+++ src/Seller/seller.js
@@ -12,6 +12,7 @@ import SearchIcon from '@mui/icons-material/Search';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import Sellerimg1 from '../Seller/Sellerimg/product-1.webp'
 import Sellerimg2 from '../Seller/Sellerimg/product-2.webp'
+import Sellerimg3 from '../Seller/Sellerimg/product-15.webp'
 function Seller() {
   return (
     // sellertitle
@@ -75,6 +76,15 @@ function Seller() {
       src={Sellerimg2} 
     
       />
+      <div className='productimg-over'>
+          <Card.Img variant="top"
+      className='juice-img'
+      
+      src={Sellerimg3} 
+    
+      />
+      </div>
+      
         <div className='product-icon'>
        <div className='product-search'> <SearchIcon className='search-icon'/></div>
        </div>
@@ -86,7 +96,10 @@ function Seller() {
         <h4 className='product-title'>STUDIO DESIGN</h4>
         <p className='product-use'>New Balance Fresh Foam ka..</p>
         <div> <StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/></div>
-        <div className='product-rate'><EuroIcon className='product-euroicon'/>18.90 <span className='secondproduct-rate'><EuroIcon className='product-euroicon'/>15.12</span></div>
+        <div className='product-rate'><EuroIcon className='product-euroicon'/>18.90 <span className='secondproduct-rate'><EuroIcon className='product-euroicon'/>15.12</span>
+        <Badge bg="secondary" className='badge-offer'>
+            <div className='percentage'>-5%</div></Badge> 
+        </div>
         <div className='product-footer'>
           <div className='add-cart'>ADD TO CART</div>
           <div className='leftside-icon'>
@@ -97,7 +110,186 @@ function Seller() {
       
     </Card>
         </div>
+         {/* third product */}
+          <div className='sellerfirst-box'>
+            <Card id='card-items' >
+       <div className='product-img'>
+      <Card.Img variant="top"
+      className='manago-img'
+      
+      src={Sellerimg1} 
+    
+      /> 
+      <div className='product-icon'>
+      <div className='product-search'> <SearchIcon className='search-icon'/></div>
+      </div>
+        <h6 className='new-box'>
+        <Badge bg="secondary" className='badge-new'>New</Badge>
+      </h6>
+      </div>
+      <Card.Body className='card-product'>
+        <h4 className='product-title'>STUDIO DESIGN</h4>
+        <p className='product-use'>Juicy Couture Juicy Quilted Ter..</p>
+        <div> <StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/></div>
+        <div className='product-rate'><EuroIcon className='product-euroicon'/>18.90 <span className='secondproduct-rate'><EuroIcon className='product-euroicon'/>34.21</span>
+          <Badge bg="secondary" className='badge-offer'>
+            <div className='percentage'>-5%</div></Badge>   
+        </div>
+        <div className='product-footer'>
+          <div className='add-cart'>ADD TO CART</div>
+          <div className='leftside-icon'>
+            <ShuffleIcon  className='cross-icon'/> &nbsp;<FavoriteBorderIcon  className='heart-icon'/>
+          </div>
+        </div>
+      </Card.Body>
+      
+    </Card>
+        </div> 
 
+        {/* fouth product  */}
+  <div className='sellerfirst-box'>
+            <Card id='card-items' >
+       <div className='product-img'>
+      <Card.Img variant="top"
+      className='manago-img'
+      
+      src={Sellerimg1} 
+    
+      /> 
+      <div className='product-icon'>
+      <div className='product-search'> <SearchIcon className='search-icon'/></div>
+      </div>
+        <h6 className='new-box'>
+        <Badge bg="secondary" className='badge-new'>New</Badge>
+      </h6>
+      </div>
+      <Card.Body className='card-product'>
+        <h4 className='product-title'>STUDIO DESIGN</h4>
+        <p className='product-use'>Juicy Couture Juicy Quilted Ter..</p>
+        <div> <StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/></div>
+        <div className='product-rate'><EuroIcon className='product-euroicon'/>18.90 <span className='secondproduct-rate'><EuroIcon className='product-euroicon'/>34.21</span>
+          <Badge bg="secondary" className='badge-offer'>
+            <div className='percentage'>-5%</div></Badge>   
+        </div>
+        <div className='product-footer'>
+          <div className='add-cart'>ADD TO CART</div>
+          <div className='leftside-icon'>
+            <ShuffleIcon  className='cross-icon'/> &nbsp;<FavoriteBorderIcon  className='heart-icon'/>
+          </div>
+        </div>
+      </Card.Body>
+      
+    </Card>
+        </div> 
+
+        {/* fifth product */}
+         <div className='sellerfirst-box'>
+            <Card id='card-items' >
+       <div className='product-img'>
+      <Card.Img variant="top"
+      className='manago-img'
+      
+      src={Sellerimg1} 
+    
+      /> 
+      <div className='product-icon'>
+      <div className='product-search'> <SearchIcon className='search-icon'/></div>
+      </div>
+        <h6 className='new-box'>
+        <Badge bg="secondary" className='badge-new'>New</Badge>
+      </h6>
+      </div>
+      <Card.Body className='card-product'>
+        <h4 className='product-title'>STUDIO DESIGN</h4>
+        <p className='product-use'>Juicy Couture Juicy Quilted Ter..</p>
+        <div> <StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/></div>
+        <div className='product-rate'><EuroIcon className='product-euroicon'/>18.90 <span className='secondproduct-rate'><EuroIcon className='product-euroicon'/>34.21</span>
+          <Badge bg="secondary" className='badge-offer'>
+            <div className='percentage'>-5%</div></Badge>   
+        </div>
+        <div className='product-footer'>
+          <div className='add-cart'>ADD TO CART</div>
+          <div className='leftside-icon'>
+            <ShuffleIcon  className='cross-icon'/> &nbsp;<FavoriteBorderIcon  className='heart-icon'/>
+          </div>
+        </div>
+      </Card.Body>
+      
+    </Card>
+        </div> 
+
+        {/* sixth product */}
+         <div className='sellerfirst-box'>
+            <Card id='card-items' >
+       <div className='product-img'>
+      <Card.Img variant="top"
+      className='manago-img'
+      
+      src={Sellerimg1} 
+    
+      /> 
+      <div className='product-icon'>
+      <div className='product-search'> <SearchIcon className='search-icon'/></div>
+      </div>
+        <h6 className='new-box'>
+        <Badge bg="secondary" className='badge-new'>New</Badge>
+      </h6>
+      </div>
+      <Card.Body className='card-product'>
+        <h4 className='product-title'>STUDIO DESIGN</h4>
+        <p className='product-use'>Juicy Couture Juicy Quilted Ter..</p>
+        <div> <StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/></div>
+        <div className='product-rate'><EuroIcon className='product-euroicon'/>18.90 <span className='secondproduct-rate'><EuroIcon className='product-euroicon'/>34.21</span>
+          <Badge bg="secondary" className='badge-offer'>
+            <div className='percentage'>-5%</div></Badge>   
+        </div>
+        <div className='product-footer'>
+          <div className='add-cart'>ADD TO CART</div>
+          <div className='leftside-icon'>
+            <ShuffleIcon  className='cross-icon'/> &nbsp;<FavoriteBorderIcon  className='heart-icon'/>
+          </div>
+        </div>
+      </Card.Body>
+      
+    </Card>
+        </div> 
+        {/* seventh product */}
+         <div className='sellerfirst-box'>
+            <Card id='card-items' >
+       <div className='product-img'>
+      <Card.Img variant="top"
+      className='manago-img'
+      
+      src={Sellerimg1} 
+    
+      /> 
+      <div className='product-icon'>
+      <div className='product-search'> <SearchIcon className='search-icon'/></div>
+      </div>
+        <h6 className='new-box'>
+        <Badge bg="secondary" className='badge-new'>New</Badge>
+      </h6>
+      </div>
+      <Card.Body className='card-product'>
+        <h4 className='product-title'>STUDIO DESIGN</h4>
+        <p className='product-use'>Juicy Couture Juicy Quilted Ter..</p>
+        <div> <StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/><StarIcon className='star'/></div>
+        <div className='product-rate'><EuroIcon className='product-euroicon'/>18.90 <span className='secondproduct-rate'><EuroIcon className='product-euroicon'/>34.21</span>
+          <Badge bg="secondary" className='badge-offer'>
+            <div className='percentage'>-5%</div></Badge>   
+        </div>
+        <div className='product-footer'>
+          <div className='add-cart'>ADD TO CART</div>
+          <div className='leftside-icon'>
+            <ShuffleIcon  className='cross-icon'/> &nbsp;<FavoriteBorderIcon  className='heart-icon'/>
+          </div>
+        </div>
+      </Card.Body>
+      
+    </Card>
+        </div> 
+
+{/* product end */}
         </div>
     </div> 
     // seller products
